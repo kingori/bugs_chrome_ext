6d7a3d22536548dc24cef97e02d94700ff2723bf
sewon ann <kingori@gmail.com>
2012-05-25 15:23:13 +0900

    음악 바뀔 때 노티 표시

--- bugs_ext_inject_player.js
+++ bugs_ext_inject_player.js
@@ -19,3 +19,23 @@ function next() {
     $('.playCtl .btns a:eq(3)').click()
   });
 }
+
+var old_song_title = '';
+
+setInterval(function () {
+  var title_element = document.getElementsByTagName("h2")[0];  
+   var new_song_title = title_element.title;
+   if (new_song_title !== old_song_title ) {
+      old_song_title = new_song_title;
+       title_element.onTitleChange();
+   }
+}, 5000);
+
+document.getElementsByTagName("h2")[0].onTitleChange = function() {
+  var title_element = document.getElementsByTagName("h2")[0];  
+  var music_info =  title_element.childNodes[1].title + " - " + title_element.title;
+  chrome.extension.sendRequest({
+        name: "showMusicInfoNoti",
+        info: music_info
+   });
+} ; 
